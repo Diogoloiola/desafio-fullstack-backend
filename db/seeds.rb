d7d71c1dd31e0fe382d8754dda9137bb076e1cfa
Diogoloiola <diogomouraloiola@gmail.com>
2021-10-23 17:10:13 -0300

    adicionando arquivo que contém os usuário e criado script que importa o json para o banco de dados

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+
+full_path_json =  Dir.pwd + '/users.json'
+
+records = JSON.parse(File.read(full_path_json))
+records['results'].each do |record|
+  user = User.new(
+      id: record['login']['uuid'],
+      gender: record['gender'],
+      title_name: record['name']['title'],
+      first_name: record['name']['first'],
+      last_name: record['name']['last'],
+      email: record['email'],
+      birth_date: record['dob']['date'],
+      age: record['dob']['age'],
+      registered_date: record['registered']['date'],
+      age_registred: record['registered']['age'],
+      phone: record['phone'],
+      cell: record['cell'],
+      id_name: record['id']['name'],
+      id_value: record['id']['value'],
+      nationality: record['nat'],
+      username: record['login']['username'],
+      password: record['login']['password'],
+      salt: record['login']['salt'],
+      md5: record['login']['md5'],
+      sha1: record['login']['sha1'],
+      sha256: record['login']['sha256']
+  )
+
+  location = Location.new(
+    street: record['location']['street'],
+    city: record['location']['city'],
+    state: record['location']['state'],
+    postcode: record['location']['postcode'],
+    latitude: record['location']['coordinates']['latitude'],
+    longitude: record['location']['coordinates']['longitude'],
+    user_id: record['login']['uuid']
+  )
+
+  picture = Picture.new(
+    large: record['picture']['large'],
+    medium: record['picture']['medium'],
+    thumbnail: record['picture']['thumbnail'],
+    user_id: record['login']['uuid']
+  )
+
+  user.save
+  location.save
+  picture.save
+end
